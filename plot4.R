1d2d56fcd6963ea2fd1f25ac31d1ee813142b7a8
sharonl5 <xiangliu01@gmail.com>
2016-12-14 17:15:15 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -9,10 +9,6 @@ datetime <- strptime(paste(subSetDsn$Date, subSetDsn$Time), format="%d/%m/%Y %H:
 globalActivePower <- as.numeric(subSetDsn$Global_active_power)
 globalReactivePower <- as.numeric(subSetDsn$Global_reactive_power)
 voltage <- as.numeric(subSetDsn$Voltage)
-subMetering1 <- as.numeric(subSetDsn$Sub_metering_1)
-subMetering2 <- as.numeric(subSetDsn$Sub_metering_2)
-subMetering3 <- as.numeric(subSetDsn$Sub_metering_3)
-
 
 png("plot4.png", width=480, height=480)
 par(mfrow = c(2, 2)) 
@@ -21,9 +17,9 @@ plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power",
 
 plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
 
-plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
-lines(datetime, subMetering2, type="l", col="red")
-lines(datetime, subMetering3, type="l", col="blue")
+plot(datetime, as.numeric(subSetDsn$Sub_metering_1), type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, as.numeric(subSetDsn$Sub_metering_2), type="l", col="red")
+lines(datetime, as.numeric(subSetDsn$Sub_metering_3), type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="n")
 
 plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")

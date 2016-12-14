dbe0a265fd27a3a7d087c20f8ea0026c2ce88117
sharonl5 <xiangliu01@gmail.com>
2016-12-14 15:26:09 -0500

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,26 @@
+# Plot 1. Data set should be in workdir with scripts.
+setwd("C:/Users/sharon.liu/Documents/R/course 4/")
+outdir<- getwd()
+
+zipF<- "./exdata_data_household_power_consumption.zip"
+dataFile <- unz(zipF, filename="household_power_consumption.txt", open = "r")
+
+# Read Data
+data <- read.csv("household_power_consumption.txt", na.string="?", sep=";")
+
+# sub set data as required
+data <- data[(data$Date=="1/2/2007" | data$Date=="2/2/2007"),]
+
+# Combine Date and Time
+data$DateTime <- strptime(paste(data$Date, data$Time, sep=" "), 
+                          format="%d/%m/%Y %H:%M:%S")
+
+# Open png device
+png("plot1.png", width=480, height=480)
+
+# Plot the graph
+hist(data$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", col="red")
+
+# Turn off device
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,23 @@
+
+
+
+setwd("C:/Users/sharon.liu/Documents/R/course 4/")
+getwd()
+
+dsn <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+##subSetDsn <- data[dsn$Date %in% c("1/2/2007","2/2/2007") ,]
+subSetDsn <- subset(dsn, dsn$Date %in% c("1/2/2007","2/2/2007"))
+
+#str(subSetData)
+datetime <- strptime(paste(subSetDsn$Date, subSetDsn$Time), format="%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetDsn$Global_active_power)
+subMetering1 <- as.numeric(subSetDsn$Sub_metering_1)
+subMetering2 <- as.numeric(subSetDsn$Sub_metering_2)
+subMetering3 <- as.numeric(subSetDsn$Sub_metering_3)
+
+png("plot3.png", width=480, height=480)
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()

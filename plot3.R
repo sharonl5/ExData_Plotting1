17738f5c066f5cc460cd35511465af2d1822abb2
sharonl5 <xiangliu01@gmail.com>
2016-12-14 15:28:12 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,14 +1,12 @@
 
 
 
-setwd("C:/Users/sharon.liu/Documents/R/course 4/")
+setwd("./Documents/R/course 4/")
 getwd()
 
 dsn <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-##subSetDsn <- data[dsn$Date %in% c("1/2/2007","2/2/2007") ,]
 subSetDsn <- subset(dsn, dsn$Date %in% c("1/2/2007","2/2/2007"))
 
-#str(subSetData)
 datetime <- strptime(paste(subSetDsn$Date, subSetDsn$Time), format="%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(subSetDsn$Global_active_power)
 subMetering1 <- as.numeric(subSetDsn$Sub_metering_1)

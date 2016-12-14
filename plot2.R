40afb2c8ac4ff7d15719cbed0971e82f5da1ec93
sharonl5 <xiangliu01@gmail.com>
2016-12-14 15:27:31 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,14 +1,14 @@
 
 
-setwd("C:/Users/sharon.liu/Documents/R/course 4/")
+setwd("./course 4/")
 getwd()
 
 dsn <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-##subSetDsn <- data[dsn$Date %in% c("1/2/2007","2/2/2007") ,]
+
 subSetDsn <- subset(dsn, dsn$Date %in% c("1/2/2007","2/2/2007"))
-#str(subSetData)
+
 datetime <- strptime(paste(subSetDsn$Date, subSetDsn$Time), format="%d/%m/%Y %H:%M:%S")
-##strptime(paste(subSetDsn$Date, subSetDsn$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+
 GAP <- as.numeric(subSetDsn$Global_active_power)
 png("plot2.png", width=480, height=480, units="px")
 plot(datetime, GAP, type="l", xlab="", ylab="Global Active Power (kilowatts)")
